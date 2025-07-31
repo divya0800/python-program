f5bc55e4dbf4006f1ac06ce9b0a9a108aad0128e
divya0800 <divyadharshini0800@gmail.com>
2025-07-31 15:42:15 +0530

    Create grade.py
    a grade

---
+++ grade.py
@@ -0,0 +1,12 @@
+python3.py
+num=int(input("Enter a Number:"))
+if(num>=90):
+    print("Grade A")
+elif(num>=80):
+    print("Grade B")
+elif(num>=70):
+    print("Grade C")
+elif(num>=60):
+    print("Grade D")
+else:
+    print("Grade E")
