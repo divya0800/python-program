1b6fe14ce4348ef78e882822a86622002610c3f7
divya0800 <divyadharshini0800@gmail.com>
2025-07-31 15:32:29 +0530

    Create fahrenheit.py
    celsius

---
+++ fahrenheit.py
@@ -0,0 +1,7 @@
+    FAHERHEIT
+
+Step1:  Start the program.
+Step2:  Read the value of celsius.
+Step3:  Calculate=(1.8*celsius+32).
+Step4:  Print the value of fahrenheit.
+Step5:  Stop the program.
