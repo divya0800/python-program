d12bc4ccf9645cb9edf455143095900f58f47a69
divya0800 <divyadharshini0800@gmail.com>
2025-07-31 15:37:08 +0530

    Create greet.py
    welcome divya

---
+++ greet.py
@@ -0,0 +1,7 @@
+greet.py
+def greet(name):
+    print('welcome',name)
+greet('divya')
+student_list=['gunasi','laya','mahi','dhanu','ashu']
+for student in student_list:
+    print('welcome',student)
